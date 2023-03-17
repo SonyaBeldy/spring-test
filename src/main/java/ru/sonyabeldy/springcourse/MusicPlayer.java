fae94d0a3872419e1ca8784a0fc7c53f9f629b95
Sonya <sofya.beldy@mail.ru>
2023-03-17 22:43:16 +1000

    Setter dependency injection

--- src/main/java/ru/sonyabeldy/springcourse/MusicPlayer.java
+++ src/main/java/ru/sonyabeldy/springcourse/MusicPlayer.java
@@ -3,12 +3,37 @@ package ru.sonyabeldy.springcourse;
 public class MusicPlayer {
     private Music music;
 
+    private String name;
+    public int volume;
+
     public MusicPlayer(Music music) {
         this.music = music;
     }
 
+    public MusicPlayer() {}
+
     public void playMusic() {
         System.out.println("Playing: " + music.getSong());
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
     }
 
 }
